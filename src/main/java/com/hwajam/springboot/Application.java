4a5fdc3ab067df224625666765befaddc982c61d
seonghyeonyun <hwajam000@gmail.com>
2020-10-03 21:46:13 +0900

    10/03/2020 commit 最初登録

---
+++ src/main/java/com/hwajam/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.hwajam.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//update user set role = 'USER';
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/hwajam/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.hwajam.springboot.web.dto;
+
+import com.hwajam.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getTitle();
+        this.author = entity.getAuthor();
+    }
+}
